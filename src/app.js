21242fabfecfb3f66595200b58a14bb2ee859e91
Paty81 <molinamartosp@gmail.com>
2025-11-24 10:08:59 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+const quien = ['Mi abuela', 'Mi madre', 'Un alien', 'Mi gato'];
+const accion = ['se comió', 'quemó', 'aterrizó en', ' rompio'];
+const que = ['mi proyecto', 'el ordenador', 'mi brazo' , 'mi casa'];
+const cuando = ['antes de salir', 'esta mañana', 'hace cinco minutos', 'mientras me duchaba']
+;
+const randomquien = quien[Math.floor(Math.random() * quien.length)];
+const randomaccion = accion[Math.floor(Math.random() * accion.length)];
+const randomque = que[Math.floor(Math.random() * que.length)];
+const randomcuando = cuando[Math.floor(Math.random() * cuando.length)];
+
+let excusa = randomquien + " " + randomaccion + " " + randomque + " " + randomcuando;
+document.getElementById("excuse").innerHTML = excusa;
+   console.log(excusa);
+
   console.log("Hello Rigo from the console!");
 };
